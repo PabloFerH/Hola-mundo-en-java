1e662a44545f3558178f144774a9a0a181e36a0d
PabloFerH <pfer9504@gmail.com>
2017-01-11 13:05:13 +0100

    Comentarios añadidos al codigo

--- Holamundo.java
+++ Holamundo.java
@@ -1,3 +1,7 @@
+/**
+*	Muestra en pantalla la frase "¡Hola Mundo!"
+*	@author Pablo Fernández Herrera
+*/
 public class HolaMundo{
 	public static void main(String[] args){
 		System.out.println("¡Hola mundo!");
