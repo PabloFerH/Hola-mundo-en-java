d640f2bd848481b82622a9061aead1c7a3997d2b
PabloFerH <pfer9504@gmail.com>
2017-01-13 13:49:06 +0100

    Saludo en colores

--- Holamundo.java
+++ Holamundo.java
@@ -7,7 +7,7 @@ public class HolaMundo{
 		String naranja  = "\033[33m";
 		String azul = "\033[34m";
 
-		System.out.println("¡Hola mundo!");
-		System.out.println("Aqui estamos haciendo pruebas de Git.");
+		System.out.println(naranja + "¡Hola mundo!");
+		System.out.println(azul + "Aqui estamos haciendo pruebas de Git.");
 	}
 }
